e625ad14480a580ed0b6360905cfb6107c49531c
Talia Betourney <teachertaliab@gmail.com>
2025-03-30 18:40:05 +0000

    db_create route - testing/deploying

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def testing():
     conn = psycopg2.connect("postgresql://talia_betourney_database_user:PiGIuxXpS96FG6tqbKT2KwLrdSeG0Hx4@dpg-cvkoc6be5dus73bsa8rg-a/talia_betourney_database")
     conn.close()
     return 'Database Connection Successful'
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://talia_betourney_database_user:PiGIuxXpS96FG6tqbKT2KwLrdSeG0Hx4@dpg-cvkoc6be5dus73bsa8rg-a/talia_betourney_database")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Successfully Created'
